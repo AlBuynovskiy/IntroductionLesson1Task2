10cc19d516494d3310ed23902678cbc879a0225d
Aliaksandr Buinouski <AlBuynovskiy@gmail.com>
2022-12-12 16:24:30 +0300

    Finish

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+*/
+
+Console.WriteLine("Введите число а");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число c");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b & a>c)
+{
+    Console.WriteLine("Максимальное значение из предложенных вариатов ");
+    Console.Write(a);
+}
+else
+{
+    if (b>a & b>c)
+    {
+       Console.WriteLine("Максимальное значение из предложенных вариатов ");
+       Console.Write(b); 
+    }
+    else
+    {
+    Console.WriteLine("Максимальное значение из предложенных вариатов ");
+    Console.Write(c);   
+    }
+}
